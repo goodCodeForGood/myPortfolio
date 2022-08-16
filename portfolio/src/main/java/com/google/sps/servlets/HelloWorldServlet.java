59620bebe47750e0873247e6236158c98f0830b9
goodCodeForGood <sahitihibane6@gmail.com>
2022-08-16 02:24:00 +0000

    Remove all comments

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -5,12 +5,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-// I added Gson library dependency to pom.xml beforehand, but 
-// I had to still import it here
 import com.google.gson.Gson; 
 
-/** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
@@ -23,24 +19,14 @@ public class HelloWorldServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // response.setContentType("text/html;");
-    // response.getWriter().println("Hello Sahiti!");
-
-    // Convert the array of messages to JSON
 
     HelloWorldServlet helloWorld = new HelloWorldServlet();
     String json = convertToJsonUsingGson(helloWorld);
 
-    // Send the JSON as the response
-    response.setContentType("application/json;"); // correct? or jsonText
+    response.setContentType("application/json;");
     response.getWriter().println(json);
   }
 
-   /**
-   * Converts a ServerStats instance into a JSON string using the Gson library. Note: We first added
-   * the Gson library dependency to pom.xml.
-   */
-  // instead of param: HelloWorldServlet messageList..
   private String convertToJsonUsingGson(HelloWorldServlet helloWorld) {
     Gson gson = new Gson();
     String json = gson.toJson(helloWorld);
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -14,36 +14,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
+
 function addRandomFact() {
-//   const greetings =
-//       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
     const facts =
       ['I was on my school badminton team.', 
       'AP Computer Science A was one of my favorite subjects in high school :)', 
       'I like to sing.', 'I love volunteering to serve communities :)', 
     'Software Engineering was a career that I considered to pursue from my childhood itself :)'];
 
-  // Pick a random fact.
   const fact = facts[Math.floor(Math.random() * facts.length)];
-
-  // Add it to the page.
   const factContainer = document.getElementById('fact-container');
   factContainer.innerText = fact;
 }
-
-/** Fetches the current date from the server and adds it to the page. */
-
-// Both the asynch fns: "showServerNoun" and "getServerMessages" are fetching 
-// from the same webseervlet called "hello" from the servlet file. I commented out
-// lines related to showServerNoun, so it doesn't show the hardcoded hello text 
-// anymore. Instead both the fns point to the same random message json strings
-
-// so is it possible to have both these fns work correctly even though they fetch
-// from the same "hello" webservlet. or would i have to create a new/another
-// webservlet to fetch from, for one of these fns.
 
 async function showServerNoun() {
     const responseFromServer = await fetch('/hello');
@@ -53,14 +35,8 @@ async function showServerNoun() {
     dateContainer.innerText = textFromResponse;
 }
 
-/* Modify your JavaScript file to fetch the JSON list from the server. 
-Confirm this works by printing it using the console.log() function. */
-
-/** Fetches messages from the server and adds them to the page. */
 async function getServerMessages() {
     const responseFromServer = await fetch('/hello');
-    // The json() function returns an object that contains fields that we can
-    // reference to create HTML.
     const myObject = await responseFromServer.json();
   
     const messagesListElement = document.getElementById('hello-container');
@@ -74,7 +50,6 @@ async function getServerMessages() {
     return messagesListElement;
   }
   
-  /** Creates an <li> element containing text. */
   function createListElement(text) {
     const liElement = document.createElement('li');
     liElement.innerText = text;
