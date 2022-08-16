61ceccacdc98f8dfbfe405bb48b6e5b194fecfab
goodCodeForGood <sahitihibane6@gmail.com>
2022-08-16 05:46:13 +0000

    remove comments

--- portfolio/src/main/java/com/google/sps/servlets/TranslationHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/TranslationHandlerServlet.java
@@ -14,17 +14,14 @@ public class TranslationHandlerServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Get the request parameters.
     String originalText = request.getParameter("text");
     String languageCode = request.getParameter("languageCode");
 
-    // Do the translation.
     Translate translate = TranslateOptions.getDefaultInstance().getService();
     Translation translation =
         translate.translate(originalText, Translate.TranslateOption.targetLanguage(languageCode));
     String translatedText = translation.getTranslatedText();
 
-    // Output the translation.
     response.setContentType("text/html; charset=UTF-8");
     response.setCharacterEncoding("UTF-8");
     response.getWriter().println(translatedText);
